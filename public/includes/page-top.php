56b1c2f8256b23d7dd7ae39095e5cdebe146b57d
CarlosRBH <carlosbeltramo2002@gmail.com>
2025-10-15 00:35:30 +0000

    primeiroapp

---
+++ public/includes/page-top.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Madruguinha</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="./css/default.css" rel="stylesheet" />
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="./img/logo.png" alt="" width="32" height="32"> Madruguinha
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMain"
+                aria-controls="navbarMain" aria-expanded="false" >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarMain">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Home Page</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="menuLink" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">Cardapio</a>
+                        <ul class="dropdown-menu" aria-labelledby="menuLink">
+                            <li>
+                                <a class="dropdown-item" href="cardapio-hamburger.php">Hamburger</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="cardapio-hotdog.php">Hot Dog</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="cardapio-potato.php">Batatas</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="cardapio-snacks.php">Petiscos</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="cardapio-bebidas.php">Bebidas</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
--- public/index.php
+++ public/index.php
@@ -1,17 +1,71 @@
-<?php
-require '../vendor/autoload.php';
+<?php require_once("./includes/page-top.php") ?>
 
-use Application\Hello;
-?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <link href="/css/default.css" rel="stylesheet" />
-    <title>Document</title>
-</head>
-<body>
-    <h1>PHP & Mysql</h1>
-    <p><?= Hello::message() ?></p>
-</body>
-</html>
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+               <img src="./img/hamburger.jpg" class="d-block" alt="..." width="700">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/cachorro.png" class="d-block" alt="..." width="700">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/batata.png" class="d-block" alt="..." width="700">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/frango.png" class="d-block" alt="..." width="700">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+
+    <main class="container p-3">
+        <div class="row">
+            <div class="col">
+                <div class="card text-white bg-warning">
+                    <img class="card-img-top" src="./img/cartoon.png" alt="Card image cap" style="width: 150px; height: auto; margin: 0 auto;">
+                    <div class="card-body">
+                        <p class="card-text">Sabor e qualidade em cada mordida. Nosso hambúrguer artesanal é feito com os melhores ingredientes, oferecendo uma experiência deliciosa e única.</p>
+                        <a href="cardapio-hamburger.php" class="btn btn-vermelho">Ver opções</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card text-white bg-warning ">
+                    <img class="card-img-top" src="./img/dogcartoon.png" alt="Card image cap" style="width: 150px; height: auto; margin: 0 auto;">
+                    <div class="card-body">
+                        <p class="card-text">O clássico hot dog, simples e irresistível. Salsicha suculenta, pão macio e o toque perfeito de condimentos. Uma delícia para qualquer momento!</p>
+                        <a href="cardapio-hotdog.php" class="btn btn-vermelho">Ver opções</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card text-white bg-warning ">
+                    <img class="card-img-top" src="./img/frita.png" alt="Card image cap" style="width: 150px; height: auto; margin: 0 auto;">
+                     <div class="card-body">
+                        <p class="card-text">Batata frita. Uma opção perfeita para quem busca algo rapido e delicioso!</p>
+                        <a href="cardapio-potato.php" class="btn btn-vermelho">Ver opções</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card text-white bg-warning ">
+                    <img class="card-img-top" src="./img/imagem.png" alt="Card image cap" style="width: 150px; height: auto; margin: 0 auto;">
+                    <div class="card-body">
+                        <p class="card-text">frango a passarinho, crocantes e com pelinhas douradas , perfeitos para acompanhar uma bebida. A combinação ideal de sabor e textura!</p>
+                        <a href="cardapio-snacks.php" class="btn btn-vermelho">Ver opções</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <?php require_once("./includes/page-botton.php") ?>
